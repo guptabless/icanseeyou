fde31ef3d27b84df726c709fcef56e6aa2db5c65
guptabless <61534368+guptabless@users.noreply.github.com>
2020-06-02 00:05:24 -0400

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,31 +1,63 @@
 import socket
-import sys
+import sys, argparse
+import bcolors
 
-print("-----------------------------------------------")
-print("Port Scanner")
-print("Code By : NG")
-print("Usage: python portscan.py and then follow the instructions")
-print("-----------------------------------------------")
+def banner():
+    print("""
+            ██████╗░░█████╗░██████╗░████████╗░██████╗░█████╗░░█████╗░███╗░░██╗
+            ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗████╗░██║
+            ██████╔╝██║░░██║██████╔╝░░░██║░░░╚█████╗░██║░░╚═╝███████║██╔██╗██║
+            ██╔═══╝░██║░░██║██╔══██╗░░░██║░░░░╚═══██╗██║░░██╗██╔══██║██║╚████║
+            ██║░░░░░╚█████╔╝██║░░██║░░░██║░░░██████╔╝╚█████╔╝██║░░██║██║░╚███║
+            ╚═╝░░░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝
+                                                                    Code by NG          
+          """)
 
-target = input("Please enter ipaddress or hostname")
-host_name = socket.gethostname()
-host_ip = socket.gethostbyname(target)
+if len(sys.argv) > 1:
+    banner()
+    if ((sys.argv[1] == '-i') | (sys.argv[1] == '-s1') | (sys.argv[1] == '-s2')):
+        try:
+            target = sys.argv[2]
+            low_range = sys.argv[4]
+            high_range = sys.argv[6]
 
-low_range = int(input("Please Enter the Port Where You want to Start Scanning"))
-high_range = int(input("Please Enter the Port Where You want to end Scanning"))
+            parser = argparse.ArgumentParser()
+            parser.add_argument("-i", required=True)
+            parser.add_argument("-s1", required=True)
+            parser.add_argument("-s2", required=True)
+            args = parser.parse_args()
 
-try:
-    for port in range(low_range,high_range):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connect = sock.connect_ex((host_ip, port))
-        if connect == 0:
-            print("Port {}:    is Open".format(port))
-        sock.close()
+            host_name = socket.gethostname()
+            host_ip = socket.gethostbyname(target)
 
-except socket.gaierror:
-    print("HostName Can't be resolved")
-    sys.exit()
+            try:
+                for port in range(int(low_range),int(high_range)):
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    connect = sock.connect_ex((host_ip, port))
+                    if connect == 0:
+                        print("Port {}:    is Open".format(port))
+                        sock.close()
+            except socket.gaierror:
+                print("HostName Can't be resolved")
+                sys.exit()
 
-except socket.error:
-    print("Server Not Found")
-    sys.exit()
+            except socket.error:
+                print("Server Not Found")
+                sys.exit()
+        except:
+            print(bcolors.ERR + 'Please provide valid Ip address ')
+            print(bcolors.BOLD + 'Please enter python portscan.py -i <valid Ip address path> -s1 <start scan range> -s2 <end scan range>')
+
+
+    elif (((sys.argv[1] == '-i') & (sys.argv[1] == '-s1')) | (sys.argv[1] == '--help')):
+        print(bcolors.BOLD + 'usage: portscan.py [-h] -i valid ip address , -s1 start scan range, -s2 end scan range ' '\n' 'OPTIONS:' '\n' '-h,--help    '
+                             'show this help message and exit' '\n''-i valid ip address  --Ip address whose open ports you want to check' '\n''-s1  starting port range,   --start range of port' 
+                             '\n' '-s2 ending port range   --end range of port ')
+    elif (((sys.argv[1] == '-i') & (sys.argv[1] == '-s1')) | (sys.argv[1] != '-s2')):
+        print('Please enter -i <valid ip address> -s1 <starting range of scanning> -s2 <ending range of scanning>')
+
+else:
+    banner()
+    print(bcolors.ERR + 'Please select options from (-i and (-s1,-s2)) or -h, with a valid domain name')
+
+
